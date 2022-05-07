b51ca689d56cfe0c59557455dc4dd8f8799a4cd7
Adeyemi Glory <adeyemiglr@gmail.com>
2022-05-07 22:35:29 +0100

    post page and form created

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,20 +1,29 @@
 import { AppBar } from "@material-ui/core";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const TopBar = () => {
+  const { pathname } = useLocation();
+  const location = pathname.split("/")[1];
+
   return (
     <AppBar className='topBar' position='fixed'>
       <div className='top-container'>
         <h2 className='logo'>AutoMart</h2>
         <nav>
           <ul>
-            <li>
+            <li
+              style={{ borderBottom: location === "" && "1px solid #221f50" }}
+            >
               <Link to='/' className='Link'>
                 <span>Home</span>
               </Link>{" "}
             </li>
-            <li>
+            <li
+              style={{
+                borderBottom: location === "post" && "1px solid #221f50",
+              }}
+            >
               {" "}
               <Link to='/post' className='Link'>
                 <span>Post Advert</span>{" "}
